a300743bfe600fd98a7104c34a837f4c33dca2b6
Jack Dent <jack@jackdent.co.uk>
2014-11-26 15:52:56 -0500

    Enum for block types

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,6 +2,8 @@ require 'mongoid'
 
 class Block
   include Mongoid::Document
+  include Mongoid::Enum
   field :name, type: String
   field :description, type: String
+  enum :type, [:text, :number, :image, :tel]
 end
