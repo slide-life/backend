f2db9dd45b8a418e30118fd817f8acf84ca7b1fc
Jack Dent <jack@jackdent.co.uk>
2014-11-28 15:43:53 -0500

    Remove unnecessary environment specification

--- config/mongoid.rb
+++ config/mongoid.rb
@@ -2,7 +2,7 @@ require 'mongoid'
 require 'moped'
 
 # Connect to the database
-Mongoid.load!("#{File.dirname(__FILE__)}/mongoid.yml", ENV['RACK_ENV'])
+Mongoid.load!("#{File.dirname(__FILE__)}/mongoid.yml")
 
 # Logging
 Moped.logger = Logger.new($stdout)
