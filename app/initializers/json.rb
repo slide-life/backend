47be7a53b5a72d9d5fd738eab45879d46477fc52
Jack Dent <jack@jackdent.co.uk>
2014-11-28 14:49:17 -0500

    Working to_json monkey patch

--- app/initializers/json.rb
+++ app/initializers/json.rb
@@ -1,16 +1,16 @@
 module BSON
   class ObjectId
-    alias :to_json :to_s
+    alias :as_json :to_s
   end
 end
 
 module Mongoid
   module Document
-    def to_json(options={})
+    def as_json(options={})
       attrs = super(options)
-      attrs['id'] = attrs['_id']
-      attrs.delete '_id' #TODO modify this to actually delete it, not for now though
-      attrs
+      json = { id: attrs['_id'] }
+      attrs.delete '_id'
+      json.merge! attrs
     end
   end
 end
