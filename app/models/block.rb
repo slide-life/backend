b32887c70b3f49db1897e3ed1197cbb4f055eb99
Jack Dent <jack@jackdent.co.uk>
2014-11-28 02:47:10 -0500

    New types

--- app/models/block.rb
+++ app/models/block.rb
@@ -5,5 +5,5 @@ class Block
   include Mongoid::Enum
   field :name, type: String
   field :description, type: String
-  enum :type, [:text, :number, :image, :tel]
+  enum :type, [:text, :number, :image, :tel, :date, :select, :list]
 end
