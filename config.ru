3a7ddf1c53e2ce02702ca0dff748366a60ead7c3
Jack Dent <jack@jackdent.co.uk>
2014-11-28 15:42:04 -0500

    Environment variables for production

--- config.ru
+++ config.ru
@@ -3,6 +3,7 @@ require 'bundler'
 
 Bundler.require
 
+require_relative 'env' if File.exists?('env.rb')
 require_relative 'config/config'
 require_relative 'app/app'
 
--- config/mongoid.rb
+++ config/mongoid.rb
@@ -2,7 +2,7 @@ require 'mongoid'
 require 'moped'
 
 # Connect to the database
-Mongoid.load!("#{File.dirname(__FILE__)}/mongoid.yml", :development)
+Mongoid.load!("#{File.dirname(__FILE__)}/mongoid.yml", ENV['RACK_ENV'])
 
 # Logging
 Moped.logger = Logger.new($stdout)
