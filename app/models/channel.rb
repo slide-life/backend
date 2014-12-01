c455b77aa1ccc194fe15c1223e973f929fc1e968
Matt Neary <neary.matt@gmail.com>
2014-12-01 01:40:02 -0500

    work better with keys

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,7 +2,7 @@ require_relative 'store'
 
 class Channel < Store
   include Mongoid::Document
-  field :key, type: String
+  field :key, type: Hash
   field :blocks, type: Array, default: []
   field :buckets, type: Array, default: []
   field :open, type: Boolean, default: true
