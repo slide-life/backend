11c713fc29c8defb592480c061e44f4d7682f0d1
Matt Neary <neary.matt@gmail.com>
2015-01-13 02:40:06 -0500

    conversation data requests made separate from instantiation and actors notified of responses

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,5 +5,14 @@ class Conversation
   field :key, type: String
   field :upstreams, type: Array, default: []
   field :downstreams, type: Array, default: []
+
+  def upstream!(payload)
+    self.upstreams << payload
+    save!
+    # NB: assumes actor
+    upstream = Actor.find(self.upstream)
+    upstream.stream(payload)
+    to_json
+  end
 end
 
--- app/routes/conversation.rb
+++ app/routes/conversation.rb
@@ -1,28 +1,54 @@
 require_relative '../models/conversation.rb'
 
 module ConversationRoutes
+  module NotificationJob
+    @queue = :default
+
+    def self.perform(params)
+      notification = Houston::Notification.new(device: params[:device])
+      notification.alert = params[:title]
+      notification.custom_data = {
+        conversation: params[:conversation],
+        blocks: params[:blocks]
+      }
+      APN.push(notification)
+    end
+  end
+
   def self.registered(app)
     app.post '/conversations' do
       key, upstream, downstream = @request_payload['key'], @request_payload['upstream'], @request_payload['downstream']
       conversation = Conversation.new(key: key, upstream: upstream, downstream: downstream)
       conversation.save!
-
-      # NB: assuming downstream is a user
-      user = User.find_by(number: downstream)
-      halt_with_error 404, 'User not found.' unless user
-
-      halt_with_error 422, 'User does not have a device registered.' if user.devices.empty?
-
-      user.devices.each do |device|
-        NotificationJob.perform(device: device, channel: conversation)
-      end
-
       conversation.to_json
     end
 
     app.put '/conversations/:id' do
       conversation = Conversation.find(params[:id])
-      halt_with_error 404, 'Conversation not found.' unless actor
+      conversation.upstream! @request_payload
+      conversation.to_json
+    end
+
+    app.post '/conversations/:id/request_content' do
+      conversation = Conversation.find(params[:id])
+      blocks = @request_payload['blocks']
+      halt_with_error 404, 'Conversation not found.' unless conversation
+
+      # NB: assuming downstream is a user
+      user = User.find_by(number: conversation.downstream)
+      halt_with_error 404, 'User not found.' unless user
+
+      halt_with_error 422, 'User does not have a device registered.' if user.devices.empty?
+
+      user.devices.each do |device|
+        NotificationJob.perform(
+          device: device,
+          conversation: conversation,
+          blocks: blocks,
+          title: "New data request")
+      end
+
+      conversation.to_json
     end
   end
 end
