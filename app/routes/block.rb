a4b00f2f0537f223cda094d1b923cc138a210094
zgao <gaoz@mit.edu>
2015-01-17 16:08:54 -0500

    Final

--- app/routes/block.rb
+++ app/routes/block.rb
@@ -1,10 +1,27 @@
 require_relative '../models/block'
 
+DEFAULT_ORGANIZATION = 'slide.life'
+
+def is_annotation(key)
+  key[0] == '_'
+end
+
+def inherits(component)
+  { '_inherits' => component }
+end
+
+def component_name(component)
+  component.split('.').last
+end
+
 def flatten(key, value, separator, fields)
   resolve_components(value)
-  fields[key] = value.select { |k, v| k[0] == '_' }
-  fields[key]['_children'] = []
-  children = value.select { |k, v| k[0] != '_' }
+
+  annotations = value.select { |k, v| is_annotation key }
+  annotations['_children'] = []
+  fields[key] = annotations
+
+  children = value.select { |k, v| ! is_annotation key }
   children.each do |k, v|
     child_name = "#{key}#{separator}#{k}"
     fields[key]['_children'] << child_name
@@ -16,7 +33,7 @@ def resolve_components(field)
   if field.has_key? '_components'
     components = field['_components']
     components.each do |component|
-      field[component.split('.').last] = { '_inherits' => component }
+      field[component_name component] = inherits component
     end
     field.delete('_components')
   end
@@ -37,35 +54,40 @@ def merge(to, from, fields)
   fields[to] = merged
 end
 
-def resolve_inheritance(key, fields)
+def resolve_inheritance(organization, key, fields)
   field = fields[key]
 
   return unless field.is_a? Hash
 
   if field.has_key? '_children'
     children = field['_children']
-    children.each { |child| resolve_inheritance(child, fields) }
+    children.each { |child| resolve_inheritance(organization, child, fields) }
   end
 
   if field.has_key? '_inherits'
     parent = field['_inherits']
-    parent = 'slide.life:' + parent unless parent.include? ':'
-    resolve_inheritance(parent, fields)
+    parent = organization + parent unless parent.include? ':'
+    resolve_inheritance(organization, parent, fields)
     merge(key, parent, fields)
     fields[key].delete('_inherits')
   end
 end
 
+def remove_children(fields)
+  fields.keys.each { |key| fields[key].delete('_children') }
+end
+
 module BlockRoutes
   def self.registered(app)
     app.get '/blocks' do
-      organization = params['organization'] || 'slide.life'
+      organization = params['organization'] || DEFAULT_ORGANIZATION
       blocks = Block.find_by(organization: organization)
       halt_with_error 404, 'Organisation not found' if blocks.nil?
 
       fields = {}
-      flatten blocks.organization, blocks.schema, ':', fields
-      fields.keys.each { |key| resolve_inheritance(key, fields) }
+      flatten(blocks.organization, blocks.schema, ':', fields)
+      fields.keys.each { |key| resolve_inheritance(organization, key, fields) }
+      remove_children fields
       fields.to_json
     end
   end
