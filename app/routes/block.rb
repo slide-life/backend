bd1be30965eb347e207bfa8e2e408643cd475cdd
Jack Dent <jack@jackdent.co.uk>
2015-01-21 23:45:58 -0500

    Use params instead of request_payload for get request

--- app/routes/block.rb
+++ app/routes/block.rb
@@ -122,7 +122,7 @@ end
 module BlockRoutes
   def self.registered(app)
     app.get '/blocks' do
-      organization = @request_payload['organization'] || DEFAULT_ORGANIZATION
+      organization = params[:organization] || DEFAULT_ORGANIZATION
       blocks = Block.find_by(organization: organization)
       halt_with_error 404, 'Organisation not found' if blocks.nil?
 
